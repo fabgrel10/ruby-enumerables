7a218ea9de61d08b98c348a1c4e4e44b8e5cd12a
andres-condezo <ra.condezomonge@gmail.com>
2022-03-31 15:57:55 -0500

    Add top level documentation and complete myList class

--- my_list.rb
+++ my_list.rb
@@ -1,12 +1,20 @@
+# frozen_string_literal: false
+
+require_relative 'my_enumerable'
+
+# MyList: This class has an instance variable @list
 class MyList
+  include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
 
-  def my_each
-    puts @list.to_s
+  def each
+    @list.each { |x| yield(x) if block_given? }
   end
 end
 
 example1 = MyList.new(1, 2, 3, 4, 5)
-example1.my_each
+example1.each { |x| puts x.to_s }
+example1.all?
