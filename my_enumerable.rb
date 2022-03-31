b19133266118bc827e98183cc4080b0492367ae5
Leonardo <albornozleo95@gmail.com>
2022-03-31 17:29:54 -0400

    Create enumerable methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: false
 
-# MyEnumerable: My enumerables methos module
+# MyEnumerable: My enumerables methods module
 module MyEnumerable
-  def all?; end
+
+  def all?
+    flag = true
+    each { |item| flag = false unless yield(item) }
+    flag
+  end
+
+  def any?
+    flag = false
+    each { |item| flag = true if yield(item) }
+    flag
+  end
+
+  def filter
+    list = []
+    each { |item| list.push(item) if yield(item)}
+    list
+  end
 end
