a2fbfa05a0c0df41cb2ec162fb5019512df6e73f
andres-condezo <ra.condezomonge@gmail.com>
2022-03-31 16:49:54 -0500

    Add Examples

--- my_list.rb
+++ my_list.rb
@@ -15,7 +15,21 @@ class MyList
   end
 end
 
-example1 = MyList.new(1, 2, 3, 4)
-example1.each { |x| puts x.to_s }
-puts example1.all? { |i| i >= 5 }.to_s
-puts example1.filter {|e| e.even?}.to_s
+list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+list.all? { |e| e < 5 }
+# => true
+list.all? { |e| e > 5 }
+# => false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# => true
+list.any? { |e| e == 5 }
+# => false
+
+# Test #filter
+list.filter { |e| e.even? }
+# => [2, 4]
